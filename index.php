442d22f4cba86c07c93ccafad4d696653003555c
softwarejhond <softwarejhond@gmail.com>
2025-01-02 17:42:50 -0500

    config files

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+include("controller/conexion.php"); ?>
+<html lang="en">
+<?php include("controller/head.php"); ?>
+<body>
+    
+
+<?php include("controller/scripts.php"); ?>
+</body>
+</html>
